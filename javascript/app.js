f044af4816c4f4d278a85f351e436970a2da5538
Greatest Dane <DaneGoodman85@gmail.com>
2018-05-23 23:34:14 -0700

    Added some old homework folders and finally got my API key!

---
+++ javascript/app.js
@@ -0,0 +1,33 @@
+// start off the page by creating a bunch of buttons from an array 
+var animals = ["Cat", "Lion", "Alligator", "Dinosaur", "Dragon", "Unicorn", "Pegasus", "Gryffin", "A real animal", "Spider", "Kermit"]
+var animalButton;
+var APIKey = "4LlS5TiESc6S7Y1FCFD5Fh7dNug7glhU";
+
+// Use a for loop to actually create the buttons
+    for (var i = 0; i < animals.length; i++) {
+        animalButton = $("<button>").attr("class", "animal-button");
+        animalButton.val(animals[i]);
+        animalButton.text(animals[i]);
+        $("#button-col").append(animalButton);
+    };
+
+
+
+//Dynamically create a new animal button using the search bar
+$("#search-button").on("click", function () {
+    event.preventDefault();
+    animalButton = $("<button>").attr("class", "animal-button");
+    animalButton.val($("#gif").val());
+    animalButton.text($("#gif").val());
+    $("#button-col").append(animalButton);
+    $("#gif").val(" ");
+});
+
+
+$(document).on("click", ".animal-button", function(){
+    alert("ANIMAL BUTTON OF: " + $(this).val());
+});
+
+
+
+
